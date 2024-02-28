00742d4807c3c5d7e26ae55d2288004950b3b289
Tom Liu <tom.liu@57blocks.com>
2024-02-28 17:55:01 +0800

    feat(monorepo v3): test running git hooks version three

---
+++ lint-staged.config.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+    '*': ['secretlint', 'prettier --cache --write --ignore-unknown'],
+    '*.{cjs,js,jsx,ts,tsx}': ['nx affected -t lint --fix --files'],
+    '*.{ts,tsx}': ['nx affected -t test --files'],
+  };
