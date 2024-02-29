93f14eb3d32a795dde3421f9ca885e1f32273184
Tom Liu <tom.liu@57blocks.com>
2024-02-29 15:47:15 +0800

    fix(prittierScript): prettier script configuration

--- apps/dashboard/src/App.tsx
+++ apps/dashboard/src/App.tsx
@@ -6,12 +6,12 @@ export const App: React.FC = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="app">
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
+    <div className='app'>
+      <header className='app-header'>
+        <img src={logo} className='app-logo' alt='logo' />
         <p>Hello Admin</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type='button' onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
         </p>
@@ -20,19 +20,19 @@ export const App: React.FC = () => {
         </p>
         <p>
           <a
-            className="app-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='app-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn React11
           </a>
           {' | '}
           <a
-            className="app-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='app-link'
+            href='https://vitejs.dev/guide/features.html'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Vite Docs
           </a>
